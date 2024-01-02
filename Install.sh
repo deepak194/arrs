998fe5bfa27331946a55f7f96a93dc08ced82572
Deepak <5906802+deepak194@users.noreply.github.com>
2024-01-03 00:34:56 +1100

    Update Install.sh
    errors

--- Install.sh
+++ Install.sh
@@ -95,13 +95,13 @@ fi
 echo "Installing Docker..."
 
 # Update package index
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 # Install prerequisites
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
 # Add Docker GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Set up the stable Docker repository for Ubuntu 22.04
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
